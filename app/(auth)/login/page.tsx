ec2b1f7d12cb2ab619cc812807d902d8dc0c6db7
Lea <lea0556769466@gmail.com>
2026-06-15 00:34:28 +0300

    הסרת שליחת מייל

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -23,7 +23,7 @@ export default async function LoginPage({ searchParams }: LoginPageProps) {
         </p>
       ) : null}
       <AuthForm mode="login" action={signIn} next={next} />
-      <ResendConfirmation />
+      {/* <ResendConfirmation /> */}
     </div>
   )
 }
